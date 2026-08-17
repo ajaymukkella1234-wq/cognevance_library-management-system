0688654da151a1861c6ef76726e0cc93b42f1fbf
ajay123 <ajaymukkella1234@gmail.com>
2026-08-17 22:22:35 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,17 +1,19 @@
-
 // Book.java
 class Book {
-    int id;
-    String title;
-    String author;
+    private int id;
+    private String title;
+    private String author;
 
-    Book(int id, String title, String author) {
+    public Book(int id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
     }
 
-    void display() {
-        System.out.println(id + " " + title + " " + author);
+    public void display() {
+        System.out.println("Book ID: " + id);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println();
     }
 }
