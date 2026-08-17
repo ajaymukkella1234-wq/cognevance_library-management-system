14ee76fc4aae2a097248ee4192abc921a2b03d2e
ajay123 <ajaymukkella1234@gmail.com>
2026-08-17 22:09:13 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,17 @@
 
+// Book.java
+class Book {
+    int id;
+    String title;
+    String author;
+
+    Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    void display() {
+        System.out.println(id + " " + title + " " + author);
+    }
+}
