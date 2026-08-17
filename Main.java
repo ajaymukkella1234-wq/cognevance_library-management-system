a99800ed50cce16d38a4f18d998f42e7773fbefd
ajay123 <ajaymukkella1234@gmail.com>
2026-08-17 22:10:47 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
+// Main.java
+class Main {
+    public static void main(String[] args) {
+        Library library = new Library();
 
+        library.addBook(new Book(1, "Java", "James"));
+        library.addBook(new Book(2, "DSA", "Mark"));
+
+        library.viewBooks();
+    }
+}
