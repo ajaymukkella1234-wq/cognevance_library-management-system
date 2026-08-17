7be4cf79ce571259e0a45951a2819300af730784
ajay123 <ajaymukkella1234@gmail.com>
2026-08-17 22:23:00 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,16 @@
 
+// Student.java
+class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void display() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Name: " + name);
+    }
+}
